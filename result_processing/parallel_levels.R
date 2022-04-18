ab26b5048e12445863b672a68cd887876823c5f3
vjayaman <vjayaman314@gmail.com>
2022-04-18 15:01:21 -0500

    Almost done, just need to add the children wherever a parent is found in the proc_0 table

--- result_processing/parallel_levels.R
+++ result_processing/parallel_levels.R
@@ -1,8 +1,8 @@
 source("result_processing/rfuncs.R")
 
 
-num_vecs <- 10
-file_index <- 2
+num_vecs <- 15
+file_index <- 3
 
 # ------------------------------------------------------------------------------
 # PARALLEL ---------------------------------------------------------------------
@@ -78,80 +78,15 @@ s_x3 <- buildLvlTbl(p_x2, p_a1, TRUE)$s_x3
 #   print(paste0("Process ", p))
 #   print(accuracy_tbl)
 # }
-parallelContainedElementsTable(s_x3, 4)
+p <- 4
+accuracy_tbl <- parallelContainedElementsTable(s_x3, p) %>% 
+  set_colnames(c("level", paste0("par_0_elements")))
 
+proc_p <- containedElementsTable(s_x3[proc == p]) %>% 
+  set_colnames(c(paste0("proc_",p,"_level"), paste0("par_", p, "_elements")))
 
-# generate table of level and absorbed elements into a group
-# designed in a sequential way
-# will use this for comparing "accuracy" for sequential and parallel groups later
-parallelContainedElementsTable <- function(s_x3, p) {
-  dif_levels <- sort(unique(s_x3$level), decreasing = TRUE)
-  for (r in dif_levels) {
-    # print(r)
-    if (r == max(unique(s_x3$level))) {
-      accuracy_tbl <- data.table(
-        level = r, elements = list(s_x3[level == r & variable != "center"]$value))
-    }else {
-      merged_pair <- s_x3[level == r & variable != "center"]
-      leaves <- merged_pair[lvl == 1]
-      if (nrow(leaves) > 0) {
-        seq_list <- list(leaves$value)
-      }else {
-        seq_list <- list()
-      }
-      # a pair merged at this level that have children
-      x1 <- merged_pair[lvl != 1]
-      if (nrow(x1) > 0) {
-        for (i in 1:nrow(x1)) {
-          seq_list <- append(seq_list, parTraceSequences(s_x3, x1[i,], r, seq_list, p))
-          df <- tibble(level = r, elements = list(unique(unlist(seq_list))))
-          accuracy_tbl <- bind_rows(accuracy_tbl, df)
-        }  
-      }else { # just merging two leaves
-        accuracy_tbl <- bind_rows(
-          accuracy_tbl, 
-          data.table(level = r, elements = list(s_x3[level == r & variable != "center"]$value)))
-      }
-    }
-  }
-  return(accuracy_tbl)
-}
-
-
-parTraceSequences <- function(s_x3, x, r, seq_list, p) {
-  if (nrow(x) > 0) {
-    for (j in 1:nrow(x)) {
-      z <- x[j,]
-      # when was this value formed in a merge?
-      # i.e. when it was a center, at a level before the cuurrent one, 
-      # and when the sequence ID was the same
-      # level_of_earlier_merge <- s_x3[variable == "center" & lvl < z$lvl & seqs == z$seqs]$level
-      
-      # arbitrarily pick the first of these (since we consider it a leaf if proc != given or 0)
-      earlier_merge <- s_x3[seqs == z$seqs & variable == "center"][1]
-      
-      # collect the triple of x1, x2, pair at this earlier level
-      y <- s_x3[level == earlier_merge$level & proc == earlier_merge$proc]
-      
-      # what was the merged pair?
-      mp <- y[variable != "center"]
-      
-      # add any children from that pair to the list if they were leaves or from a different proc
-      seq_list <- append(seq_list, mp[lvl == 1 & proc %in% c(0, p)]$value)
-      seq_list <- append(seq_list, mp[lvl == 1 & !(proc %in% c(0,p))]$value)
-      # if they were not leaves, then we want to trace back to find when they 
-      # were merged into an internal node -- will keep doint this until we have 
-      # added all leaves to the list
-      z <- mp[lvl != 1 & proc %in% c(0, p)]
-      seq_list <- append(seq_list, parTraceSequences(s_x3, z, r, seq_list, p))
-    }
-  }else {
-    # print(list(unlist(seq_list)))
-    return(list(unlist(seq_list)))
-  }
-  return(seq_list)
-}
-
+# now just need to add the elements from proc 4 to any element list of 
+# accuracy table that contains the parent of those children
 
 
 
